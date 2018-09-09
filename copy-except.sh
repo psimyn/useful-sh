6f56fc9af257f0f680e091a20d1de0dedb5535ef
Simon Knox <psimyn@users.noreply.github.com>
2018-09-09 17:41:27 +1000

    Update copy-except.sh

--- copy-except.sh
+++ copy-except.sh
@@ -1,1 +1,2 @@
+# https://stackoverflow.com/questions/4585929/how-to-use-cp-command-to-exclude-a-specific-directory
 # rsync -av --progress sourcefolder /destinationfolder --exclude thefoldertoexclude
